c36ea683fe3723fda404c749a094e7cfa6796d98
Alex <infanti.alexandre@gmail.com>
2018-08-08 14:24:42 +0200

    triste vie

--- formulaire.php
+++ formulaire.php
@@ -6,109 +6,110 @@
 
 
 <div class="jumbotron jumbotron-sm">
-    <div class="container">
+  <div class="container">
 
-        <div class="row">
+    <div class="row">
 
-            <div class="col-sm-12 col-lg-12">
-                <h1 class="h1">
+      <div class="col-sm-12 col-lg-12">
+        <h1 class="h1">
                     Contactez nous<small><br>Feel free to contact us</small></h1>
 
-            </div>
-         </div>
-     </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <hr color="yellow">
 
-<a class="bouton" href="index.php"> <bold> Retour vers le site </bold> </a>
+<a class="bouton" href="index.php">
+  <bold> Retour vers le site </bold>
+</a>
 
 <hr color="yellow">
 
 
-<form  action="function.php" method="post" id="form">
+<form>
 
-      <div class="form-group">
-<div class="container">
-    <div class="row">
+  <div class="form-group">
+    <div class="container">
+      <div class="row">
         <div class="col-md-8">
-            <div class="well well-sm">
-                <form>
-                <div class="row">
-                    <div class="col-md-7">
-                        <div class="form-group">
-                            <label name="nom">
+          <div class="well well-sm">
+            <form action="function.php" method="post" id="form">
+              <div class="row">
+                <div class="col-md-7">
+                  <div class="form-group">
+                    <label name="nom">
                                 Nom</label>
-                            <input name= type="text" class="form-control" id="nom" placeholder="Enter name" required="required" />
-                        </div>
-                           <div class="col-sm-4">
-                             <div class="form-group">
-                               <label class="control-label " for="genre">Genre :</label>  <br>
-                             <input type="radio" id="genre" name="genre" label="Monsieur"> M.
-                             <input type="radio" id="genre" name="genre" label="Madame"> Mme
-                           </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">
+                    <input name=t ype="text" class="form-control" id="nom" placeholder="Enter name" required="required" />
+                  </div>
+                  <div class="col-sm-4">
+                    <div class="form-group">
+                      <label class="control-label " for="genre">Genre :</label> <br>
+                      <input type="radio" id="genre" name="genre" label="Monsieur"> M.
+                      <input type="radio" id="genre" name="genre" label="Madame"> Mme
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="email">
                                 Adresse email</label>
-                            <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
-                                </span>
-                                <input type="email" name="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
-                        </div>
-                        <div class="form-group">
-                            <label for="subject">
+                    <div class="input-group">
+                      <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                      </span>
+                      <input type="email" name="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
+                  </div>
+                  <div class="form-group">
+                    <label for="subject">
                                 Raison</label>
-                            <select id="objet" name="object" class="form-control" required="required">
+                    <select id="objet" name="object" class="form-control" required="required">
                                 <option value="na" selected="">Choississez:</option>
                                 <option value="service">Plainte Service</option>
                                 <option value="suggestions">Suggestions</option>
                                 <option value="technique">Question technique</option>
                                 <option value="autre">autre</option>
                             </select>
-                        </div>
+                  </div>
 
-                         <div class="form-group">
-                            <label class="control-label " for="formatRep">Format de la réponse souhaitée :</label>
-                            <div class="col-sm-6">
-                              <input type="radio" id="formatRep" name="formatRep" label="HTML"> HTML
-                              <input type="radio" id="formatRep" name="formatRep" label="Text"> Text
-                            </div>
-                         </div>
+                  <div class="form-group">
+                    <label class="control-label " for="formatRep">Format de la réponse souhaitée :</label>
+                    <div class="col-sm-6">
+                      <input type="radio" id="formatRep" name="formatRep" label="HTML"> HTML
+                      <input type="radio" id="formatRep" name="formatRep" label="Text"> Text
+                    </div>
+                  </div>
 
-                         &nbsp
-                         <div class="form-group">
-                            <label class="control-label " for="document"></label>
-                            <div >
-                              <input type="file" id="document" name="file" value="">
-                            </div>
-                          </div>
+                  &nbsp
+                  <div class="form-group">
+                    <label class="control-label " for="document"></label>
+                    <div>
+                      <input type="file" id="document" name="file" value="">
                     </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label name="prenom">
-                                Prénom</label>
-                            <input name= type="text" class="form-control" id="prenom" placeholder="Enter firstname" required="required" />
-                        </div>
-                      </div>
-                    <div class="col-md-5">
-                        <div class="form-group">
-                            <label for="name">
-                                Message</label>
-                            <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
-                                placeholder="Message"></textarea>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
-                            ENVOYER</button>
-                    </div>
+                  </div>
                 </div>
-                </form>
-            </div>
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <label name="prenom">
+                                Prénom</label>
+                    <input name=t ype="text" class="form-control" id="prenom" placeholder="Enter firstname" required="required" />
+                  </div>
+                </div>
+                <div class="col-md-5">
+                  <div class="form-group">
+                    <label for="name">
+                                Message</label>
+                    <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required" placeholder="Message"></textarea>
+                  </div>
+                </div>
+                <div class="col-md-12">
+                  <button type="submit" name="submit" class="btn btn-primary pull-right" id="btnContactUs">
+                            ENVOYER</button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
         <div class="col-md-4 align-self-center text-center justify-center">
-            <form>
+          <form>
             <legend><span class="glyphicon glyphicon-globe align-middle"></span> Notre maison mère</legend>
             <address class="align-middle">
                 <strong>Les petits riens</strong><br>
@@ -117,15 +118,17 @@
                 Ouvert du lundi au samedi
             </address>
 
-            </form>
+          </form>
         </div>
+      </div>
     </div>
-</div>
 
-<hr color="yellow">
+    <hr color="yellow">
 
-<a class="bouton" href="index.php"> <bold> Retour vers le site </bold> </a>
+    <a class="bouton" href="index.php">
+      <bold> Retour vers le site </bold>
+    </a>
 
-<div class="text-center align-items-center" >
-<img  class= "rounded mx-auto d-block" src="img/logo.png" alt="">
-</div>
+    <div class="text-center align-items-center">
+      <img class="rounded mx-auto d-block" src="img/logo.png" alt="">
+    </div>
--- function.php
+++ function.php
@@ -3,117 +3,71 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
 
 
-if($_POST){
- $nom =  $_POST['nom'];
- $prenom = $_POST['prenom'];
- $mail =  $_POST['email'] ;
- $objet =  $_POST['object'] ;
- $message = $_POST['message'] ;
- $document = $_POST['file'] ;
- $formatRep =  $_POST['formatRep'] ;
 
-    echo $nom . '<br>';
-    echo $prenom . '<br>';
-    echo $mail . '<br>';
-    echo $objet . '<br>';
-    echo $message . '<br>';
-    echo $document . '<br>';
-    echo $formatRep . '<br>';
-}
-
- // Filtrage des données
- function verif_formulaire(){
-   $nom_san = filter_var($nom, FILTER_SANITIZE_STRING);
-   $prenom_san = filter_var($prenom, FILTER_SANITIZE_STRING).
-   $email_san = filter_var($email, FILTER_SANITIZE_EMAIL);
-   $message_san = filter_var($message, FILTER_SANITIZE_STRING);
-   $email_valid = filter_var($email_san, FILTER_VALIDATE_EMAIL);
- };
-
-
-
-
-/**
- * This example shows settings to use when sending via Google's Gmail servers.
- * This uses traditional id & password authentication - look at the gmail_xoauth.phps
- * example to see how to use XOAUTH2.
- * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
- */
 //Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
-require 'vendor/autoload.php';
-//Create a new PHPMailer instance
+require '../vendor/autoload.php';
 $mail = new PHPMailer;
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 2;
-//Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
-// use
-// $mail->Host = gethostbyname('smtp.gmail.com');
-// if your network does not support SMTP over IPv6
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 587;
-//Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = 'tls';
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "jujubidoubidou@gmail.com";
-//Password to use for SMTP authentication
-$mail->Password = "jujubidoubidoubi";
-//Set who the message is to be sent from
-$mail->setFrom('', 'Ju Bi');
-//Set an alternative reply-to address
-$mail->addReplyTo('jujubidoubidou@gmail.com', 'Les petits riens');
-//Set who the message is to be sent to
-$mail->addAddress('jujubidoubidou@gmail.com', 'Lespetitesrien');
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->Body = 'This is the html message';
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-//Attach an image file
 
 
+  if (isset($_POST['submit'])) {
+
+    //Create a new PHPMailer instance
+    //Tell PHPMailer to use SMTP
+    $mail->isSMTP();
+    //Enable SMTP debugging
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    $mail->SMTPDebug = 2;
+    //Set the hostname of the mail server
+    $mail->Host = 'smtp.gmail.com';
+    // use
+    // $mail->Host = gethostbyname('smtp.gmail.com');
+    // if your network does not support SMTP over IPv6
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 587;
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = "jujubidoubidou@gmail.com";
+    //Password to use for SMTP authentication
+    $mail->Password = "jujubidoubidoubi";
+
+    //Set who the message is to be sent from
+    $mail->setFrom('jujubidoubidou@gmail.com', 'Juju et Alex');
 
 
+    $email =  $_POST['email'] ;
+    $prenom = $_POST['prenom'];
+    //Set who the message is to be sent to
+    $mail->addAddress($email, $prenom );
+    //Set the subject line
+    $mail->Subject = 'PHPMailer GMail SMTP test';
+    //Read an HTML message body from an external file, convert referenced images to embedded,
+    //convert HTML into a basic plain-text alternative body
+    $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+    //Replace the plain text body with one created manually
+    $mail->AltBody = 'This is a plain-text message body';
+    //Attach an image file
+    $mail->addAttachment('images/phpmailer_mini.png');
+    //send the message, check for errors
+    if (!$mail->send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+        echo "Message sent!";
+        //Section 2: IMAP
+        //Uncomment these to save your message in the 'Sent Mail' folder.
+        #if (save_mail($mail)) {
+        #    echo "Message saved!";
+        #}
+    }
 
-$mail->addAttachment('img/logo.png');
-//send the message, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-    //Section 2: IMAP
-    //Uncomment these to save your message in the 'Sent Mail' folder.
-    #if (save_mail($mail)) {
-    #    echo "Message saved!";
-    #}
-}
 
+  }
 
-//Section 2: IMAP
-//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
-//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
-//You can use imap_getmailboxes($imapStream, '/imap/ssl') to get a list of available folders or labels, this can
-//be useful if you are trying to get this working on a non-Gmail IMAP server.
-function save_mail($mail)
-{
-    //You can change 'Sent Mail' to any other folder or tag
-    $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
-    //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
-    $imapStream = imap_open($path, $mail->Username, $mail->Password);
-    $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-    imap_close($imapStream);
-    return $result;
-}
 
 
  ?>
